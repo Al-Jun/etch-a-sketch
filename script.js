c34265936d6ec57bb7477d96f1b06107a7f8a3bb
Al-Jun <izzuddinaljundy7@gmail.com>
2022-05-12 08:42:12 +0700

    Add color variable

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const colorButton = document.querySelector('#color-input');
 const gridSlider = document.querySelector('#grid-size');
 const etchBox = document.querySelector('.etch-box');
 const box = document.createElement('div');
+
+let color = colorButton.value;
