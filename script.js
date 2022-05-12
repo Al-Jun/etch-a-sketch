f5ccca2f15dba19c5df6534a1a6bdb28fca0d7d8
Al-Jun <izzuddinaljundy7@gmail.com>
2022-05-12 08:41:47 +0700

    Add box Variable

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const colorButton = document.querySelector('#color-input');
 const gridSlider = document.querySelector('#grid-size');
 const etchBox = document.querySelector('.etch-box');
+const box = document.createElement('div');
