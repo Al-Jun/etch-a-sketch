e65aa545c4e0bc7887a38a163c556fad1c89ab24
Al-Jun <izzuddinaljundy7@gmail.com>
2022-05-15 09:52:10 +0700

    Remove box variable

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const colorButton = document.querySelector('#color-input');
 const gridSlider = document.querySelector('#grid-size');
 const etchBox = document.querySelector('.etch-box');
-const box = document.createElement('div');
 
 let color = colorButton.value;
 let grid = gridSlider.value;
